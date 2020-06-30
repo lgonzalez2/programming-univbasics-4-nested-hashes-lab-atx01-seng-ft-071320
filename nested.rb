2dbdfa9fc53bcd238524f36953ae591bb072cb81
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-06-30 23:53:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,9 +85,6 @@ def adding_matz
 end
 
 def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-  # return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -128,5 +125,9 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  
+  progammer_hash
 
 end
